5811404b73a045e15044ed3113561c4a056274df
あおいそら <85457566+CaesiusSky@users.noreply.github.com>
2021-06-07 16:40:18 +0900

    Add files via upload

--- content_scripts.js
+++ content_scripts.js
@@ -3,7 +3,6 @@ const forceBrowserDefault = function(e){
   return true;
 };
 function CancelReadOnly(){
-    //要素を取得
     var EKUserAnswer = document.getElementsByTagName('textarea')
 	var EKfinalAnswer = document.getElementsByClassName("nan-qa-correct")
 	for (let i=0;i<EKUserAnswer.length;i++){
@@ -16,15 +15,8 @@ function CancelReadOnly(){
 	
 var alcBodyARRAY = document.getElementsByTagName("html")
 var alcbody = alcBodyARRAY[0]
-function domChange(){
-    console.log("変化しました！");
-}
-/*
-let observer = new MutationObserver(domChange);
-let ;
-observer.observe(alcBody, config);*/
-//window.addEventListener("load", domChange, false)
-var ELKLObserve = new MutationObserver(domChange)
+
+var ELKLObserve = new MutationObserver(CancelReadOnly)
 const config = {childList: true,
 				subtree: true,
 				characterData: true,
@@ -32,11 +24,10 @@ const config = {childList: true,
 				}
 
 
-/*ELKL.browser.runtime.onMessage.addListener(({ active }) => {
-  if (active) {*/
+
     document.addEventListener('beforecopy', forceBrowserDefault, true);
 	document.addEventListener('beforecut', forceBrowserDefault, true);
-	//document.addEventListener('click', forceBrowserDefault, true);
+
 	document.addEventListener('contextmenu', forceBrowserDefault, true);
 	document.addEventListener('copy', forceBrowserDefault, true);
 	document.addEventListener('dragstart', forceBrowserDefault, true);
@@ -49,42 +40,4 @@ const config = {childList: true,
 	CancelReadOnly
 	ELKLObserve.observe(alcbody,config)
 
-	/*
 
-	//ストリーム変更時にいいねを消し去る
-function ObserveStream(){
-    //オブザーバーの作成
-    var observer = new MutationObserver(CancelReadOnly);
-    //監視の開始
-    observer.observe(document.getElementsByTagName('textarea')[0], {
-        attributes: true,
-        childList:  true
-    });
-    CancelReadOnly();
-} 
-//body変更時にObserveStreamを設定する。
-//オブザーバーの作成
-var observer = new MutationObserver(ObserveStream);
-//監視の開始
-observer.observe(document.getElementsByTagName('textarea'), {
-    attributes: true
-});
-*/
-
- /* } else {    
-    document.removeEventListener('beforecopy', forceBrowserDefault, true);
-	document.removeEventListener('beforecut', forceBrowserDefault, true);
-	document.removeEventListener('click', forceBrowserDefault, true);
-	document.removeEventListener('contextmenu', forceBrowserDefault, true);
-	document.removeEventListener('copy', forceBrowserDefault, true);
-	document.removeEventListener('dragstart', forceBrowserDefault, true);
-	document.removeEventListener('mousedown', forceBrowserDefault, true);
-	document.removeEventListener('mouseup', forceBrowserDefault, true);
-    document.removeEventListener('cut', forceBrowserDefault, true);
-    document.removeEventListener('paste', forceBrowserDefault, true);
-	document.removeEventListener('selectstart', forceBrowserDefault, true);
-
-  }
-});
-*/
-//ELKL.browser.runtime.sendMessage({ didLoad: true });

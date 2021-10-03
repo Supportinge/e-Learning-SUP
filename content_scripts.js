5b93fd577ac20d41eacc5d371a5837276a4de86a
e-ラン　サポータ <85457566+Supportinge@users.noreply.github.com>
2021-10-04 03:42:07 +0900

    Ver. 1.2.1.0
    解答の記録に関する致命的なバグ(従来型は解答記録が冒頭一単語になるバグがあった)の修正とそれに伴う動作仕様の変更

--- content_scripts.js
+++ content_scripts.js
@@ -1,34 +1,47 @@
+//1.2.1.0
 const forceBrowserDefault = function(e){
-  e.stopImmediatePropagation();
-  return true;
+	e.stopImmediatePropagation();
+	return true;
 };
+var EKUA;
+var EKFA;
 function CancelReadOnly(){
     var EKUserAnswer = document.getElementsByTagName('textarea')
 	var EKfinalAnswer = document.getElementsByClassName("nan-qa-correct")
 	try{
-	for (let i=0;i<EKUserAnswer.length;i++){
-			EKUserAnswer[i].readOnly = false;
-			EKUserAnswer[i].value = EKfinalAnswer[i].innerHTML
-			EKUserAnswer[i].dispatchEvent(new Event('change'))
+		for (let i=0;i<EKUserAnswer.length;i++){
+			EKUserAnswer[i].addEventListener('click',CLICKKAITO,false)
+			//EKUserAnswer[i].readOnly = false;
+			EKUA=EKUserAnswer[i];
+			EKFA=EKfinalAnswer[i];
+
+
 		}
-	}catch{void(0);}
-    }
-	
-var alcBodyARRAY = document.getElementsByTagName("html")
-var alcbody = alcBodyARRAY[0]
+		var AllALCItem = document.children;
+		console.log("GETALLTAG");
+		for(i=0; i<AllALCItem.length; i++){
+			AllALCItem[i].removeAttribute("disabled");
+			console.log("DISABELED");
 
-var ELKLObserve = new MutationObserver(CancelReadOnly)
-const config = {childList: true,
-				subtree: true,
-				characterData: true,
-				subtree: true
-				}
+		}
+    }catch{console.log("ERROR");}
+		
+}
+	var alcBodyARRAY = document.getElementsByTagName("html")
+	var alcbody = alcBodyARRAY[0]
 
+	var ELKLObserve = new MutationObserver(CancelReadOnly);
+	const config = {childList: true,subtree: true,characterData: true,subtree: true}
+ function CLICKKAITO(){
+	 EKUA.value = EKFA.innerHTML
+			EKUA.dispatchEvent(new Event('change'))
+	 
+	 
+ }
 
 
     document.addEventListener('beforecopy', forceBrowserDefault, true);
 	document.addEventListener('beforecut', forceBrowserDefault, true);
-
 	document.addEventListener('contextmenu', forceBrowserDefault, true);
 	document.addEventListener('copy', forceBrowserDefault, true);
 	document.addEventListener('dragstart', forceBrowserDefault, true);
@@ -38,16 +51,16 @@ const config = {childList: true,
     document.addEventListener('paste', forceBrowserDefault, true);
 	document.addEventListener('selectstart', forceBrowserDefault, true);
 	document.addEventListener('click', CancelReadOnly, false);
-	document.addEventListener('fullscreenchange',function(){
-		if(document.fullscreenElement != null){document.exitFullscreen();}else{;};},true)
+	document.addEventListener('fullscreenchange',function(){if(document.fullscreenElement != null){document.exitFullscreen();}else{;};},true);
+	console.log("EvScreenOK")
 	CancelReadOnly;
 	ELKLObserve.observe(alcbody,config);
-	;
 	
+if(false){	
 var NOTPOPUPALC = document.getElementsByTagName("a"); 
-	for (i=0;i<NOTPOPUPALC.length;i++) { 
-        if (NOTPOPUPALC[i].target=="_blank" /* aggressive mode goes here */) { 
-            NOTPOPUPALC[i].target="_top" /* _self vs _top */
-        } 
-	}
-
+for (i=0;i<NOTPOPUPALC.length;i++) { 
+if (NOTPOPUPALC[i].target=="_blank" ) { 
+    NOTPOPUPALC[i].target="_top" 
+    } 
+}
+}else{;};
